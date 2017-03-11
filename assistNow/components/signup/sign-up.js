ead9bbe1ccf666af833d639888d702ba2c0e3ae4
Hieu Nguyen <hieutnguyen28@gmail.com>
2017-03-11 14:27:45 -0800

    add sign-up page

---
+++ assistNow/components/signup/sign-up.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { 
+	Text,
+	View,
+	Button
+} from 'react-native';
+
+export default class sign-up extends Component {
+	render(){
+		return(
+			<View>
+				<Text>Create Cliet Page</Text>
+			</View>
+		);
+	}
+}
