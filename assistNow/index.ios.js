ce0bf724eef6922d9fe9788b577d8c72130d202b
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-03-11 11:56:32 -0800

    added navigator and component foloders

--- assistNow/index.ios.js
+++ assistNow/index.ios.js
@@ -20,13 +20,7 @@ export default class assistNow extends Component {
         <Text style={styles.welcome}>
           Welcome to Assist Now!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        
       </View>
     );
   }
