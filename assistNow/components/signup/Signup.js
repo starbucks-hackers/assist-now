c02fea19e413e308dfbdbaf63932a1c1c9080ddf
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-03-11 18:27:19 -0800

    resolve merge

--- assistNow/components/signup/Signup.js
+++ assistNow/components/signup/Signup.js
@@ -1,39 +1,53 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Button
+	StyleSheet,
+	Text,
+	View,
+	Button
 } from 'react-native';
 
-export default class Signup extends Component {
-    onPress(){
-        this.props.navigator.push({
-            id: 'addChild'
-        });
-    }
-    render(){
-        return (
-        <View style={styles.container}>
-            <Button
-                style={styles.button}
-                title="Sign up"
-                onPress={this.onPress.bind(this)}
-            />
-        </View>
-        );
-    }
+
+
+export default class SignUp extends Component {
+
+	render(){
+		return(
+			<View>
+				<Text style={styles.signUpTitle}>Create Profile</Text>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-    constainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'green' 
-    },
-    button: {
-        borderColor: 'green'
-    }
+	signUpTitle: {
+		backgroundColor: 'rgba( 0, 121, 189, 0.85)',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2
+	}
 });
+
+
+
+//Goal:
+// import SignUp1 from './components/signup/SignUpP1';
+// import SignUp2 from './components/signup/SignUpP2';
+
+// export default class SignUp extends Component {
+// 	const { signUpStyle } = styles;
+
+// 	render(){
+// 		return(
+// 			<View>
+// 				<Text style={signUpStyle}>Create Profile</Text>
+// 				// <SignUpP1 />
+// 				// <SignUpP2 />
+// 			</View>
+// 		);
+// 	}
+// }
--- assistNow/index.android.js
+++ assistNow/index.android.js
@@ -13,10 +13,12 @@ import {
   Navigator
 } from 'react-native';
 
-import AddChild from './components/add-child/add-child'
-import SignUp from './components/signup/Signup'
-import AddChild2 from './components/add-child/add-child2'
-import Home from './components/home/Home'
+
+import Home from './components/home/Home';
+import SignUp from './components/signup/SignUp';
+import AddChild from './components/add-child/add-child';
+import AddChild2 from './components/add-child/add-child2';
+
 
 
 export default class assistNow extends Component {
@@ -24,7 +26,7 @@ export default class assistNow extends Component {
     switch(route.id) {
       case 'home':
         return (<Home navigator={navigator} />);
-      case 'signUp':
+      case 'signup':
         return (<SignUp navigator={navigator} />);
       case 'addChild': 
         return (<AddChild navigator={navigator} />);
