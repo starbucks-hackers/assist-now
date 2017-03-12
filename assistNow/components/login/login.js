1e3500942ca546d8e02f4b2d420d1cc81bfae341
Henry Qi <henryqi222@gmail.com>
2017-03-11 17:26:20 -0800

    login comp

---
+++ assistNow/components/login/login.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Navigator
+} from 'react-native';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <View>
+        <Text>From Login</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
